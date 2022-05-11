68375cf7e956897ce87c5bf76e5e405a13f29cef
Richard Seal <richyseal@gmail.com>
2022-05-11 13:26:01 -0400

    Add the new SugconEu project to the solution

--- src/Project/SugconEuSite/rendering/src/Layout.tsx
+++ src/Project/SugconEuSite/rendering/src/Layout.tsx
@@ -26,15 +26,6 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
       </Head>
 
-      {/*
-        VisitorIdentification is necessary for Sitecore Analytics to determine if the visitor is a robot.
-        If Sitecore XP (with xConnect/xDB) is used, this is required or else analytics will not be collected for the JSS app.
-        For XM (CMS-only) apps, this should be removed.
-
-        VI detection only runs once for a given analytics ID, so this is not a recurring operation once cookies are established.
-      */}
-      <VisitorIdentification />
-
       <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
       <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
