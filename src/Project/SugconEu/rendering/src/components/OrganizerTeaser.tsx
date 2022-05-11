9026d9a1a43fd9812fa7c9b62260fa1985fff081
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-05-11 08:34:35 -0700

    Fixing prettier errors

--- src/Project/SugconEu/rendering/src/components/OrganizerTeaser.tsx
+++ src/Project/SugconEu/rendering/src/components/OrganizerTeaser.tsx
@@ -1,15 +1,23 @@
-import { Text, Field,ImageField,Image,LinkField, Link,withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  Text,
+  Field,
+  ImageField,
+  Image,
+  LinkField,
+  Link,
+  withDatasourceCheck,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 type OrganizerTeaserProps = ComponentProps & {
   fields: {
-   // Image: ImageField;
-   // Name: Field<string>;
-   // LinkedIn: Field<string>;
-   // Twitter: Field<string>;
-   // LinkedInLink: LinkField;
-   // TwitterLink: LinkField;
-   Organizers: OrganizerProps[];
+    // Image: ImageField;
+    // Name: Field<string>;
+    // LinkedIn: Field<string>;
+    // Twitter: Field<string>;
+    // LinkedInLink: LinkField;
+    // TwitterLink: LinkField;
+    Organizers: OrganizerProps[];
   };
 };
 export type OrganizerProps = ComponentProps & {
@@ -20,35 +28,37 @@ export type OrganizerProps = ComponentProps & {
     Twitter: Field<string>;
     LinkedInLink: LinkField;
     TwitterLink: LinkField;
-  }
+  };
 };
 
-const OrganizerTeaser = ({fields}: OrganizerTeaserProps): JSX.Element => (
-
-  <div className='row'>
-      {fields?.Organizers?.length == 0 ? (
-          <div>No Organizers</div>
-        ) : (
-          fields?.Organizers?.map((Organizer) => {
-              return (
-                <div className='col-12 col-md-3 organizer' >
-                  <div className='card'>
-                    <p><Image field={Organizer?.fields?.Image}/></p>
-                    <div className='cardBody'>
-                      <h3><Text field={Organizer?.fields?.Name} /></h3>
-                      <p>
-                        <Link field={Organizer?.fields?.LinkedInLink}></Link><br/>
-                        <Link field={Organizer?.fields?.TwitterLink}></Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              )
-            }
-          )
-        )
-      }
-
+const OrganizerTeaser = ({ fields }: OrganizerTeaserProps): JSX.Element => (
+  <div className="row">
+    {fields?.Organizers?.length == 0 ? (
+      <div>No Organizers</div>
+    ) : (
+      fields?.Organizers?.map((Organizer) => {
+        return (
+          // eslint-disable-next-line react/jsx-key
+          <div className="col-12 col-md-3 organizer">
+            <div className="card">
+              <p>
+                <Image field={Organizer?.fields?.Image} />
+              </p>
+              <div className="cardBody">
+                <h3>
+                  <Text field={Organizer?.fields?.Name} />
+                </h3>
+                <p>
+                  <Link field={Organizer?.fields?.LinkedInLink}></Link>
+                  <br />
+                  <Link field={Organizer?.fields?.TwitterLink}></Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      })
+    )}
   </div>
 );
 
--- src/Project/SugconEu/rendering/src/components/RichText.tsx
+++ src/Project/SugconEu/rendering/src/components/RichText.tsx
@@ -1,15 +1,14 @@
-import {Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 
 type MyComponentProps = {
   fields: {
-    
-    Text: Field<string>  
+    Text: Field<string>;
   };
-}
+};
 
 const MyComponent = (props: MyComponentProps): JSX.Element => (
-  <div className='row richtext'>
-    <div className ='col-12'>
+  <div className="row richtext">
+    <div className="col-12">
       <RichText field={props.fields.Text} />
     </div>
   </div>
