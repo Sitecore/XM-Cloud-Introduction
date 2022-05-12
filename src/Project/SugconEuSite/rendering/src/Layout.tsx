6d31b92dab56f7d90aaaf728f0ca3fc2c778aada
Richard Seal <richyseal@gmail.com>
2022-05-12 09:37:15 -0400

    Fix prettier bugs in Layout

--- src/Project/SugconEuSite/rendering/src/Layout.tsx
+++ src/Project/SugconEuSite/rendering/src/Layout.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import {
-  Placeholder,
-  VisitorIdentification,
-  getPublicUrl,
-  LayoutServiceData,
-} from '@sitecore-jss/sitecore-jss-nextjs';
-import Navigation from 'src/Navigation';
+import { Placeholder, getPublicUrl, LayoutServiceData } from '@sitecore-jss/sitecore-jss-nextjs';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
@@ -22,11 +16,10 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
   return (
     <>
       <Head>
-        <title>{route?.fields?.pageTitle?.value || 'Page'}</title>
+        <title>YES - {route?.fields?.pageTitle?.value || 'Page'}</title>
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
       </Head>
 
-      <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
       <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
     </>
