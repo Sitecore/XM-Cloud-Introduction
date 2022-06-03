08a123af6930773515530e934ea2891bf1a87acf
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-06-03 11:24:18 -0700

    Styling Hero component hard coding images for now

--- src/Project/Sugcon/SugconAnzSxa/src/components/Sugon/Hero.tsx
+++ src/Project/Sugcon/SugconAnzSxa/src/components/Sugon/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, Field, ImageField, Image } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Text, Field, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import Image from 'next/image';
 
 interface Fields {
   Text: Field<string>;
@@ -24,19 +25,19 @@ const Hero = (props: HeroProps): JSX.Element => (
 export const Default = (props: HeroProps): JSX.Element => {
   if (props.fields) {
     return (
-      <div className="container-fluid">
-        <div className="row" id="box-search">
-          <div className="thumbnail">
-            <Image media={props.fields.BackgroundImage} className="img-fluid heroimage"></Image>
-            <div className="caption d-none d-md-block">
-              <p className="hero-label">
-                <Text field={props.fields.Label} />
-              </p>
-              <p className="h1 py-4">
-                <Text className="contentTitle" field={props.fields.Text} />
-              </p>
-            </div>
-          </div>
+      <div className="mb-3 d-flex justify-content-center">
+        <Image
+          src="https://mvp-cd.sitecore.com/-/media/Sugcon/EU/budapest-view.jpg?h=900&iar=0&w=1600&hash=743F1E41D91667BF68372E39A71AEBB3"
+          width="1600"
+          height="900"
+        />
+        <div className="caption d-none d-lg-block">
+          <p className="hero-label">
+            <Text field={props.fields.Label} />
+          </p>
+          <p className="h1 py-4">
+            <Text className="contentTitle" field={props.fields.Text} />
+          </p>
         </div>
       </div>
     );
