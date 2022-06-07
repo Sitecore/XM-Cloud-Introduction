354ba8e197632a47a901a046892a4d9909537d8c
lovesitecore <sebastian.winter@sitecore.com>
2022-06-07 18:48:57 +0200

    Styling Sponsor List, OrganiserList main, header and footer

--- src/Project/Sugcon/SugconAnzSxa/src/components/Sugon/SponsorList.tsx
+++ src/Project/Sugcon/SugconAnzSxa/src/components/Sugon/SponsorList.tsx
@@ -30,26 +30,34 @@ const SponsorListDefaultComponent = (props: SponsorListProps): JSX.Element => (
 export const Default = (props: SponsorListProps): JSX.Element => {
   if (props.fields) {
     return (
-      <div className="container component">
-        <div className="row">
-          <div className='col-12 className="SponsorImage2"'>
-            <h3>
-              <Text field={props.fields?.Category} />
-            </h3>
-            {props.fields?.Sponsors?.length == 0 ? (
-              <div>No Organizers</div>
-            ) : (
-              props.fields?.Sponsors?.map((Sponsor) => {
-                return (
-                  <div key={Sponsor.fields.SponsorName.value} className="SponsorBlock">
-                    <Text field={Sponsor.fields.SponsorName} />
-                    <div className="SponsorImage">
-                      <Image field={Sponsor?.fields.SponsorLogo} />
-                    </div>
-                  </div>
-                );
-              })
-            )}
+      <div className={`component sponsor-list default ${props.params.styles}`}>
+        <div className="component-content container">
+          <div className="row">
+            <div className='col-12 className="SponsorImage2"'>
+              <h3>
+                <Text field={props.fields?.Category} />
+              </h3>
+              <div className="row">
+              {props.fields?.Sponsors?.length == 0 ? (
+                <div>No Organizers</div>
+              ) : (
+                props.fields?.Sponsors?.map((Sponsor) => {
+                  return (
+                    
+                      <div className="col-6 col-md-4 col-lg-3 SponsorBlock">
+                        
+                          <Text field={Sponsor.fields.SponsorName} />
+                          <div className="SponsorImage">
+                            <Image field={Sponsor?.fields.SponsorLogo} />
+                          </div>
+                        
+                      </div>
+                    
+                  );
+                })
+              )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -62,36 +70,42 @@ export const Default = (props: SponsorListProps): JSX.Element => {
 export const Platinum = (props: SponsorListProps): JSX.Element => {
   if (props.fields) {
     return (
-      <div className="container component">
-        <h3>
-          <Text field={props.fields?.Category} />
-        </h3>
-        {props.fields?.Sponsors?.length == 0 ? (
-          <div>No Organizers</div>
-        ) : (
-          props.fields?.Sponsors?.map((Sponsor) => {
-            return (
-              <div key={Sponsor.fields.SponsorName.value} className="row">
-                <div className="col-12 col-md-6">
-                  <p className="sponsorLabel">
-                    <Text field={props?.fields?.Category} />
-                  </p>
-                  <h2 className="sponsorName">
-                    <Text field={Sponsor?.fields?.SponsorName} />
-                  </h2>
-                  <br />
-                  <Text field={Sponsor?.fields?.SponsorUrl} />
-                  <br />
-                </div>
-                <div className="col-12 col-md-6 sponsorTeaserImg ">
-                  <div className="sponsorTeaserImg">
-                    <Image field={Sponsor?.fields?.SponsorLogo} />
+      <div className={`component sponsor-list platinum ${props.params.styles}`}>
+        <div className="component-content container">
+          <h3>
+            <Text field={props.fields?.Category} />
+          </h3>
+          {props.fields?.Sponsors?.length == 0 ? (
+            <div>No Organizers</div>
+          ) : (
+            props.fields?.Sponsors?.map((Sponsor) => {
+              return (
+                <div className="row">
+                  <div className="col-12 col-md-6">
+                    <p className="sponsorLabel">
+                      <Text field={props?.fields?.Category} />
+                    </p>
+                    <h2 className="sponsorName">
+                      <Text field={Sponsor?.fields?.SponsorName} />
+                    </h2>
+                    <br />
+                    <Text field={Sponsor?.fields?.SponsorUrl} />
+                    <br />
+                  </div>
+                  <div className="col-12 col-md-6">
+                    <div className="sponsorImgPlatinumOuter">
+                      <div className="sponsorImgPlatinum">
+                        <picture>
+                        <Image field={Sponsor?.fields?.SponsorLogo} />
+                        </picture>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        )}
+              );
+            })
+          )}
+        </div>
       </div>
     );
   }
