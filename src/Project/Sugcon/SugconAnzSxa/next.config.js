d884a5de33d37e4fa9bf6e7167be02c11e266f70
Rob Earlam <robert.earlam@gmail.com>
2022-06-07 12:58:06 +1000

    Temporarily disabled type checking on nextjs apps

--- src/Project/Sugcon/SugconAnzSxa/next.config.js
+++ src/Project/Sugcon/SugconAnzSxa/next.config.js
@@ -52,6 +52,14 @@ const nextConfig = {
       },
     ];
   },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true
+  }
 };
 
 module.exports = () => {
