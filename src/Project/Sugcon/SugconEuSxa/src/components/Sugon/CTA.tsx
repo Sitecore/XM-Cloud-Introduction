09ff3da2ffe1216d3e5e14b90a9310e79fa0c334
Richard Seal <richyseal@gmail.com>
2022-06-09 14:18:56 -0400

    Add bootstrap icons

--- src/Project/Sugcon/SugconEuSxa/src/components/Sugon/CTA.tsx
+++ src/Project/Sugcon/SugconEuSxa/src/components/Sugon/CTA.tsx
@@ -31,35 +31,24 @@ const CTA = (props: CTAProps): JSX.Element => (
 export const Default = (props: CTAProps): JSX.Element => {
   if (props.fields) {
     return (
-      <div className="container component">
-        <div className="row justify-content-end cta">
-          <div className="col-12 col-lg-7">
-            <div className="ctaText">
-              <h3>
-                <Text field={props.fields.Headline} />
-              </h3>
-              <p>
-                <RichText field={props.fields.Text} />
-              </p>
+      <div className="container component mt-5">
+        <div className="d-flex flex-row">
+          <div className="col-12 col-md-5 p-5 align-self-stretch bg-light">
+            <h3 className="pb-4">
+              <Text field={props.fields.Headline} />
+            </h3>
+            <div className="pb-3 fs-5">
+              <RichText field={props.fields.Text} />
+            </div>
+            <div className="text-dark fs-5 fw-bold">
               <Link field={props.fields.Link}></Link>
+              <i className="bi bi-arrow-right"></i>
             </div>
           </div>
-          <div className="floated ctaImage" style={{ opacity: 1, top: 0 }}>
+          <div className="align-self-stretch">
             <Image field={props.fields.Image} />
           </div>
         </div>
-      </div>
-    );
-  }
-  return <CTA {...props} />;
-};
-
-export const Test = (props: CTAProps): JSX.Element => {
-  if (props.fields) {
-    return (
-      <div>
-        <p>CTA Component</p>
-        <Text field={props.fields.Headline} />
       </div>
     );
   }
