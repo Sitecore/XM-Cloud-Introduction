234cd7dc489801522004b9d63564076f52e43881
Ivan Lieckens <ivanlieckens@hotmail.com>
2022-09-30 17:07:46 +0200

    Route => LayoutViewModel conversion

---
+++ src/Project/MvpSite/rendering/Models/LayoutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.AspNet.RenderingEngine.Binding.Attributes;
+using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Mvp.Project.MvpSite.Models
+{
+    public class LayoutViewModel
+    {
+        [SitecoreRouteField]
+        public TextField MenuTitle { get; set; }
+
+        [SitecoreRouteField]
+        public CheckboxField IncludeInMenu { get; set; }
+
+        [SitecoreRouteField]
+        public CheckboxField RequiresAuthentication { get; set; }
+    }
+}
