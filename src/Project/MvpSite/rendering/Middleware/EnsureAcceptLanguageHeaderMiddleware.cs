61b7e31bf7a96efdad6eac661aae8c3f024b8ad1
Rob Earlam <robert.earlam@gmail.com>
2022-10-27 14:05:51 +1100

    Added middleware to ensure Accept-Language header is present

---
+++ src/Project/MvpSite/rendering/Middleware/EnsureAcceptLanguageHeaderMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Mvp.Project.MvpSite.Middleware
+{
+    public class EnsureAcceptLanguageHeaderMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RenderingEngineMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The next middleware to call.</param>
+        /// <param name="requestMapper">The <see cref="ISitecoreLayoutRequestMapper"/> to map the HttpRequest to a Layout Service request.</param>
+        /// <param name="layoutService">The layout service client.</param>
+        /// <param name="options">Rendering Engine options.</param>
+        public EnsureAcceptLanguageHeaderMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            this.next = next;
+            this.configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (!httpContext.Request.Headers.ContainsKey("Accept-Language"))
+            {
+                var defaultLang = configuration.GetValue<string>("DefaultAcceptLanguageHeader");
+                httpContext.Request.Headers.Add("Accept-Language", defaultLang);
+            }
+
+            await next(httpContext).ConfigureAwait(false);
+        }
+    }
+}
