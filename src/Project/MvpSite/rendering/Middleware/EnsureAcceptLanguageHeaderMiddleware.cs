7248a90f08abdf72b7478da7f543105c0187fb0e
Rob Earlam <robert.earlam@gmail.com>
2022-10-27 14:08:02 +1100

    Removed unneeded comments

--- src/Project/MvpSite/rendering/Middleware/EnsureAcceptLanguageHeaderMiddleware.cs
+++ src/Project/MvpSite/rendering/Middleware/EnsureAcceptLanguageHeaderMiddleware.cs
@@ -9,13 +9,6 @@ namespace Mvp.Project.MvpSite.Middleware
         private readonly RequestDelegate next;
         private readonly IConfiguration configuration;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RenderingEngineMiddleware"/> class.
-        /// </summary>
-        /// <param name="next">The next middleware to call.</param>
-        /// <param name="requestMapper">The <see cref="ISitecoreLayoutRequestMapper"/> to map the HttpRequest to a Layout Service request.</param>
-        /// <param name="layoutService">The layout service client.</param>
-        /// <param name="options">Rendering Engine options.</param>
         public EnsureAcceptLanguageHeaderMiddleware(RequestDelegate next, IConfiguration configuration)
         {
             this.next = next;
