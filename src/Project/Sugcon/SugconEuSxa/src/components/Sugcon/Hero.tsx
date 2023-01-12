8db73829286bf2142fb69ab7002eee654db833ef
Rob Earlam <robert.earlam@gmail.com>
2023-01-13 09:23:47 +1100

    Added display logic wrapping link field for hero

--- src/Project/Sugcon/SugconEuSxa/src/components/Sugcon/Hero.tsx
+++ src/Project/Sugcon/SugconEuSxa/src/components/Sugcon/Hero.tsx
@@ -48,7 +48,11 @@ export const Default = (props: HeroProps): JSX.Element => {
                 </h1>
                 <div className="hero-text">
                   <JssRichText field={props.fields.Description} />
-                  <Link className="link-button primary" field={props.fields.Link}></Link>
+                  {props.fields.Link ? (
+                    <Link className="link-button primary" field={props.fields.Link}></Link>
+                  ) : (
+                    ''
+                  )}
                 </div>
               </div>
             </div>
