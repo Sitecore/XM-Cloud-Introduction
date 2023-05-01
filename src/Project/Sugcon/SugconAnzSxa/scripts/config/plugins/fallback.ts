ba910e5a2d31a257fc4c74d815709a8145a05705
Rob Earlam <robert.earlam@gmail.com>
2023-05-02 08:46:17 +1000

    Updated fallback script for SUGCON sites

--- src/Project/Sugcon/SugconAnzSxa/scripts/config/plugins/fallback.ts
+++ src/Project/Sugcon/SugconAnzSxa/scripts/config/plugins/fallback.ts
@@ -12,6 +12,7 @@ class FallbackPlugin implements ConfigPlugin {
     return Object.assign({}, config, {
       defaultLanguage: config.defaultLanguage || 'en',
       sitecoreApiKey: config.sitecoreApiKey || 'no-api-key-set',
+      sitecoreApiHost: config.sitecoreApiKey || '',
     });
   }
 }
