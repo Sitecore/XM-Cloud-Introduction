d93f9c87e137122e5b2c9545c3f1d87c74979d41
Eric Tallant <etallant@degdigital.com>
2024-02-14 17:57:52 -0600

    create accordion component and story, and add sucgon theme colors blue and gray

---
+++ src/Project/Sugcon2024/Sugcon/src/stories/components/Basic Components/Accordion.stories.ts
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Default as Accordion } from '../../../components/Basic Components/Accordion';
+
+const meta = {
+  title: 'Components/Accordion',
+  component: Accordion,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Accordion>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const element = {
+  fields: {
+    Title: {
+      value:
+        'Donec mauris diam, finibus gravida dolor et, accumsan auctor augue. Aliquam erat volutpat. Nullam fringilla suscipit lectus?',
+    },
+    Text: {
+      value:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a metus imperdiet ante tempor lacinia id id turpis. Sed vel diam sit amet mauris eleifend suscipit. Mauris odio risus, consequat in aliquet eget, tempus id mi. Quisque vestibulum ac enim fermentum gravida.',
+    },
+  },
+};
+
+export const Event: Story = {
+  name: 'Accordion',
+  args: {
+    params: {
+      styles: '',
+    },
+    fields: {
+      Title: {
+        value: 'FAQs',
+      },
+      AccordionElementList: new Array(4).fill(element),
+    },
+  },
+};
