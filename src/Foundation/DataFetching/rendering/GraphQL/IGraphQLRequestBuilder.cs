4dbe9a6c73e2d52f374f0a72ec4e4be087cd69c4
Ivan Lieckens <ivanlieckens@hotmail.com>
2024-02-16 12:36:57 +0100

    Cleanup
    + Nuget dependency updates
    + Removed Foundation DataFetching

--- src/Foundation/DataFetching/rendering/GraphQL/IGraphQLRequestBuilder.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mvp.Foundation.DataFetching.GraphQL
-{
-    public interface IGraphQLRequestBuilder
-    {
-        public GraphQLHttpRequestWithHeaders BuildRequest(string query, object variables);
-    }
-}
