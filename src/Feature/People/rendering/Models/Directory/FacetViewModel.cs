19761a67519c1a48e399b1a09f18690e85a2c899
Ivan Lieckens <ivanlieckens@hotmail.com>
2024-02-19 22:28:17 +0100

    (WIP) Unstyled Version w Test Data

---
+++ src/Feature/People/rendering/Models/Directory/FacetViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Mvp.Feature.People.Models.Directory
+{
+    public class FacetViewModel
+    {
+        public string Identifier { get; set; }
+        
+        public TextField Name { get; set; }
+
+        public List<FacetOption> FacetOptions { get; set; } = [];
+    }
+}
