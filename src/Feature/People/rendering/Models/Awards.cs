4a3cf6d83ce0e490495b3a67a03bf57cf1e281e5
Ivan Lieckens <ivan.lieckens@sitecore.com>
2024-02-20 15:54:30 +0100

    MVP Changes Recommit

---
+++ src/Feature/People/rendering/Models/Awards.cs
@@ -0,0 +1,10 @@
+﻿using Mvp.Foundation.DataFetching.GraphQL.Models;
+
+namespace Mvp.Feature.People.Models;
+
+public class Awards
+{
+    public NameItem Parent { get; set; }
+
+    public TargetItemFieldValueItem Field { get; set; }
+}
