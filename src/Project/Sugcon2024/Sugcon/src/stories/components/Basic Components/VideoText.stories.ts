8ead8b1992ad44d1a263a462e73fd2af4ca97439
Eric Sanner <eric.sanner@perficient.com>
2024-02-22 15:50:47 -0500

    Create video text component

---
+++ src/Project/Sugcon2024/Sugcon/src/stories/components/Basic Components/VideoText.stories.ts
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Default as VideoText } from '../../../components/Basic Components/VideoText';
+const meta = {
+  title: 'Components/VideoText',
+  component: VideoText,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof VideoText>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const VideoAndText: Story = {
+  name: 'Video and Text',
+  args: {
+    params: {
+      styles: '',
+    },
+    fields: {
+      Headline: {
+        value: 'Sitecore Sessions',
+      },
+      Text: {
+        value:
+          "Megan Jensen leads a panel discussion with Perficient's Newest Sitecore MVPs: Emily Lord, Eric Sanner and Tiffany Laster about their experience in becoming new MVPs",
+      },
+      YoutubeVideoId: {
+        value: 'uPyuI5UtmJc',
+      },
+      TextHeadline: {
+        value: 'Episode 4',
+      },
+    },
+  },
+};
