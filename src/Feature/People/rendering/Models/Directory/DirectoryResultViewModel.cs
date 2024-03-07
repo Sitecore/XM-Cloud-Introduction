952c270dc0a23d1d6f982f6d15f603388386e616
Ivan Lieckens <ivanlieckens@hotmail.com>
2024-03-07 18:21:13 +0100

    New MVP Directory Search
    + Styling
    + Gravatar query string

--- src/Feature/People/rendering/Models/Directory/DirectoryResultViewModel.cs
+++ src/Feature/People/rendering/Models/Directory/DirectoryResultViewModel.cs
@@ -28,6 +28,10 @@ namespace Mvp.Feature.People.Models.Directory
             result.Type = last.MvpType.Name;
             result.Country = profile.Country?.Name;
             result.Image = profile.ImageUri ?? new Uri("/images/mvp-base-user-grey.png", UriKind.Relative);
+            if (result.Image.IsAbsoluteUri && result.Image.Host == "www.gravatar.com")
+            {
+                result.Image = new Uri(result.Image.OriginalString + "?s=250&d=https%3A%2F%2Fmvp.sitecore.net%2Fimages%2Fmvp-base-user-grey.png");
+            }
             result.Year = last.Application.Selection.Year.ToString();
             if (baseUri != null && Uri.TryCreate(baseUri, profile.Id.ToString("N"), out Uri profileUri))
             {
