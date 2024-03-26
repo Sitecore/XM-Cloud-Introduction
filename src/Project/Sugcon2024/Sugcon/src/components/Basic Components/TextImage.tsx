f15c8638542059fbfaec220ebfe97d4ea908eb61
PerficientDave <Dave.Ambrose@Perficient.com>
2024-03-25 21:31:57 -0400

    Updates to Text Image for margins, image size, and border radius to match design

--- src/Project/Sugcon2024/Sugcon/src/components/Basic Components/TextImage.tsx
+++ src/Project/Sugcon2024/Sugcon/src/components/Basic Components/TextImage.tsx
@@ -28,29 +28,40 @@ export type TextImageProps = {
 
 export const Default = (props: TextImageProps): JSX.Element => {
   return (
-    <Flex direction={{ base: 'column', md: 'row' }} w="100%" maxW="1366px" m="auto" pt={20}>
-      <Flex
-        direction="column"
-        margin="0 auto" // Center the content box
-        flexGrow={1}
-        minWidth="50%"
-      >
-        <Box width="auto" alignSelf="end" maxWidth="683px" minWidth="360px">
+    <Flex
+      direction={{ base: 'column', md: 'row' }}
+      w="100%"
+      margin="0 auto"
+      maxW="1366px"
+      pt={20}
+      px={{ base: '0', md: '20px' }}
+      position="relative"
+      overflow="hidden"
+    >
+      <Flex direction="column" mx={{ base: '20px', md: 'auto' }} flexGrow={1} minWidth="50%">
+        <Box
+          width="auto"
+          alignSelf="end"
+          maxWidth={{ base: 'auto', md: '683px' }}
+          minWidth="360px"
+          pr={{ base: '0', md: '20px' }}
+        >
           <Heading as="h2" fontSize="30px" fontWeight="bold" mb="33px">
             <JssText field={props.fields.Headline} />
           </Heading>
           <Text as={JssRichText} mb={6} fontSize="18px" field={props.fields.Text} />
         </Box>
       </Flex>
-      <Box flex="1" position="relative" minWidth="50%" maxHeight="400px" alignItems="center">
+      <Box flex="1" position="relative" minWidth="50%" maxHeight="100%" alignItems="center">
         {' '}
         <Image
           as={JssImage}
           src={props.fields.Image?.value?.src}
           //alt={props.fields.Image?.value?.alt}
-          width="400px"
-          height="100%"
-          borderRadius={15}
+          width="100%"
+          height="auto"
+          maxWidth={{ base: 'auto', md: '500px' }}
+          borderRadius={{ base: '0', md: '15' }}
           margin="0 auto"
           field={props.fields.Image}
         />
