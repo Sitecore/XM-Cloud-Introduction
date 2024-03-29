ea48b800b389c5c6a9a917e3e5ad0be7e1df3275
cheeto-bandito <chet.potvin@merkle.com>
2024-03-28 21:00:46 -0500

    Fixed linting problem with comment

--- src/Project/Sugcon2024/Sugcon/src/pages/_app.tsx
+++ src/Project/Sugcon2024/Sugcon/src/pages/_app.tsx
@@ -14,9 +14,11 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
 
   return (
     <>
-      // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
-      // Note Next.js does not (currently) provide anything for translation, only i18n routing.
-      // If your app is not multilingual, next-localization and references to it can be removed.
+      {/*
+       Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
+       Note Next.js does not (currently) provide anything for translation, only i18n routing.
+       If your app is not multilingual, next-localization and references to it can be removed.
+      */}
       <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
         <Component {...rest} />
       </I18nProvider>
