744140471f055a16fabc4b43393289fb93aa6203
cheeto-bandito <chet.potvin@merkle.com>
2024-03-28 20:49:34 -0500

    Added code for enabling Speed Insights

--- src/Project/Sugcon2024/Sugcon/src/pages/_app.tsx
+++ src/Project/Sugcon2024/Sugcon/src/pages/_app.tsx
@@ -1,19 +1,28 @@
 import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import 'assets/main.scss';
 
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
 
+  // Vercel Speed Insights - Turned on and off through env variable.
+  const isSpeedInsightsEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SPEED_INSIGHTS === 'true';
+
   return (
-    // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
-    // Note Next.js does not (currently) provide anything for translation, only i18n routing.
-    // If your app is not multilingual, next-localization and references to it can be removed.
-    <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
-      <Component {...rest} />
-    </I18nProvider>
+    <>
+      // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
+      // Note Next.js does not (currently) provide anything for translation, only i18n routing.
+      // If your app is not multilingual, next-localization and references to it can be removed.
+      <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
+        <Component {...rest} />
+      </I18nProvider>
+
+      {isSpeedInsightsEnabled ? <SpeedInsights /> : <></>}
+    </>
   );
 }
 
