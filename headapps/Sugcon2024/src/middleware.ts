549237cde47cfdf442b53d2a5bef503bbfcc65b5
Rob Earlam <robert.earlam@gmail.com>
2026-01-09 13:32:17 +1100

    Updated middleware

--- headapps/Sugcon2024/src/middleware.ts
+++ headapps/Sugcon2024/src/middleware.ts
@@ -45,6 +45,7 @@ const redirects = new RedirectsMiddleware({
    */
   sites,
   ...scConfig.api.edge,
+  ...scConfig.api.local,
   ...scConfig.redirects,
   // This function determines if the middleware should be turned off on per-request basis.
   // Certain paths are ignored by default (e.g. Next.js API routes), but you may wish to disable more.
