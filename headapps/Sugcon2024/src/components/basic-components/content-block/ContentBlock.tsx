ea794491b39e2503c88f1ee421746a8b0f39216a
Rob Earlam <robert.earlam@gmail.com>
2026-04-02 10:35:53 +1100

    Copilot PR Review changes

--- headapps/Sugcon2024/src/components/basic-components/content-block/ContentBlock.tsx
+++ headapps/Sugcon2024/src/components/basic-components/content-block/ContentBlock.tsx
@@ -1,5 +1,5 @@
 import { JSX } from 'react';
-import { Text, RichText, Field, withDatasourceCheck } from '@sitecore-content-sdk/nextjs';
+import { Text, RichText, Field } from '@sitecore-content-sdk/nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 type ContentBlockProps = ComponentProps & {
